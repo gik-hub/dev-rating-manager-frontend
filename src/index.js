15436bb6f116706e2c599c23fcbec6654eecfa56
Titus Thumbi <gikandi95@gmail.com>
2019-10-28 13:32:24 +0200

    add project setup modules for development

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function Hi() {
+    debugger;
+    return <p> Hello React </p>
+}
+
+render( <Hi />, document.getElementById('app'));
