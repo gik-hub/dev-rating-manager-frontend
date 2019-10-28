a3c1542b68b0758376c8edc0e0e9bccf94aa4064
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-10-28 20:03:38 +0200

    feature(adding a navBar): there should be a navigation bar for the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route , Switch } from 'react-router-dom';
+import Header from './shared/Header';
+import HomePage from './HomePage';
+import ProfilePage from './ProfilePage';
+import NotFoundPage from './shared/NotFound';
 import '../styles/App.scss';
 
 const App = () => {
-    return (
-        <h1> Hello React </h1>
+    return (<>
+        <Header/>
+        <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/profile" component={ProfilePage}/>
+            <Route component={NotFoundPage}/>
+        </Switch>
+        </>
     )
 }
 
---
+++ src/components/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage = () => {
+    return(
+    <div>
+        <h1>Your Profile</h1>
+        <p>Check your dev pulse rating from here</p>
+    </div>)
+}
+
+export default ProfilePage;
